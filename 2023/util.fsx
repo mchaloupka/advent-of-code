04c060318c2983813c2f5e471cb0c06c0f9e535c
Milos Chaloupka <chaloupkamilos@hotmail.com>
2023-12-05 12:44:13 +0000

    Update util.fsx

--- 2023/util.fsx
+++ 2023/util.fsx
@@ -1,3 +1,4 @@
+open System
 open System.IO
 
 let readLines (filePath:string) = seq {
@@ -5,3 +6,11 @@ let readLines (filePath:string) = seq {
     while not sr.EndOfStream do
         yield sr.ReadLine ()
 }
+
+let splitIntoTwo character (text: string) =
+    let split = text.Split([|character|])
+    split.[0], split.[1]
+
+let splitBy (character: char) (text: string) =
+    let split = text.Split([|character|], StringSplitOptions.RemoveEmptyEntries)
+    split |> List.ofArray
