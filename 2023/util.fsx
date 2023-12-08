addbf8684f15311c958aa184b305d9738a7d1d51
Milos Chaloupka <chaloupkamilos@hotmail.com>
2023-12-08 11:19:26 +0000

    Update util.fsx

--- 2023/util.fsx
+++ 2023/util.fsx
@@ -1,5 +1,6 @@
 open System
 open System.IO
+open System.Text.RegularExpressions
 
 let readLines (filePath:string) = seq {
     use sr = new StreamReader (filePath)
@@ -14,3 +15,14 @@ let splitIntoTwo character (text: string) =
 let splitBy (character: char) (text: string) =
     let split = text.Split([|character|], StringSplitOptions.RemoveEmptyEntries)
     split |> List.ofArray
+
+let splitByRegexGroups (regex: string) (text:string) =
+    let rx = Regex(regex, RegexOptions.None)
+    let m = rx.Match text
+    
+    if m.Success then m.Groups |> Seq.skip 1 |> Seq.map (fun x -> x.Value) |> Seq.toList
+    else failwithf "Regex '%s' does not match '%s'" regex text
+
+let extractFirstLine lines =
+    if lines |> Seq.isEmpty then failwith "Empty sequence"
+    else lines |> Seq.head, lines |> Seq.skip 1
