8e0a745bedaef9d4762e488edd13bc954260a589
Milos Chaloupka <cmilos@amazon.com>
2025-12-07 13:45:28 -0800

    Add remaining days

--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -13,6 +13,12 @@ mod day03;
 mod day04;
 mod day05;
 mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
 
 fn day_func(day: i32) -> fn(&str) {
     match day {
@@ -22,6 +28,12 @@ fn day_func(day: i32) -> fn(&str) {
         4 => day04::run,
         5 => day05::run,
         6 => day06::run,
+        7 => day07::run,
+        8 => day08::run,
+        9 => day09::run,
+        10 => day10::run,
+        11 => day11::run,
+        12 => day12::run,
         _ => unreachable!(),
     }
 }
